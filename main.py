25490efceb26537a62f018546520bce7d63794ef
mohammad afsar <35555773+afsarahmad786@users.noreply.github.com>
2018-11-13 14:35:00 +0530

    Add files via upload
    second commit

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 
 # window = Tk()
 
-def showsubmit():
-    root = Tk()
-    root.messagebox.showinfo("Message", "Some message you want the users to see")
-    mainloop()
+
 def Login():
 
     root = Tk()
@@ -16,7 +14,7 @@ def Login():
 
 
     label_1 = Label(root, text="Username",fg="red")
-    label_2 = Label(root, text="Password",fg="red")
+    label_2 = Label(root, text="Registration no",fg="red")
 
     entry_1 = Entry(root)
     entry_2 = Entry(root)
@@ -28,11 +26,26 @@ def Login():
 
 
 
-    checkbox = Checkbutton(root, text="Keep me logged in")
-    checkbox.grid(columnspan=2)
-    MyButton1 = Button(root, text="Submit", width=10,fg="white",bg="blue" ,command=showsubmit)
+
+    def submit():
+        name = entry_1.get()
+        name2 = entry_2.get()
+        with open(name+".txt", "r") as rf:
+            with open(name2 + ".txt", "r") as nf:
+                name = entry_1.get()
+                name2 = entry_2.get()
+                line=rf.readline(5)
+                line2=nf.readline(10)
+                if(name==line) or (name2==line2):
+                    messagebox.showinfo("Message", "thanks for login")
+                else:
+                    messagebox.showinfo("Message", "please register first")
+
+            rf.close()
+        root.destroy()
+    MyButton1 = Button(root, text="Submit", width=10,fg="white",bg="blue" ,command=submit)
     MyButton1.grid(row=3, column=0, sticky=E, columnspan=2)
-    MyButton1 = Button(root, text="new user", width=10,fg="white",bg="blue")
+    MyButton1 = Button(root, text="new user", width=10,fg="white",bg="blue",command=Register)
     MyButton1.grid(rowspan=5)
     mainloop()
 
@@ -42,8 +55,8 @@ def consignment():
 
     root.title("Welcome to Track Consignment")
 
-    label_1 = Label(root, text="Mobile No.",fg="red")
-    label_2 = Label(root, text="Consignment No.",fg="red")
+    label_1 = Label(root, text="Name.",fg="red")
+    label_2 = Label(root, text="Registration No.",fg="red")
 
     entry_1 = Entry(root)
     entry_2 = Entry(root)
@@ -52,10 +65,35 @@ def consignment():
     label_2.grid(row=1, sticky=E)
     entry_1.grid(row=0, column=1)
     entry_2.grid(row=1, column=1)
-    MyButton1 = Button(root, text="Submit", width=10,bg="blue",fg="white")
-    MyButton1.grid(columnspan=3)
 
+    def submit():
+
+        name = entry_1.get()
+        name2 = entry_2.get()
+        with open(name + ".txt", "r") as rf:
+            with open(name2 + ".txt", "r") as nf:
+                name = entry_1.get()
+                name2 = entry_2.get()
+                if (name=='' and name2==''):
+                    messagebox.showinfo("Message", 'enter values')
+                line_new=rf.readline(5)
+                line = rf.readlines()
+
+                line2 = nf.readline(10)
+                if (name == line_new) or (name2 == line2):
+                    messagebox.showinfo("Message", line)
+                else:
+                    messagebox.showinfo("Message", 'register first')
+
+            rf.close()
+        root.destroy()
+    MyButton1 = Button(root, text="Track", width=10,fg="white",bg="blue" ,command=submit)
+    MyButton1.grid(row=3, column=0, sticky=E, columnspan=2)
+    MyButton1 = Button(root, text="new user", width=10,fg="white",bg="blue",command=Register)
+    MyButton1.grid(rowspan=5)
     mainloop()
+
+
 
 
 def Register():
@@ -68,11 +106,15 @@ def Register():
     label_2 = Label(root, text="Reg. No",fg="red")
     label_3 = Label(root, text="mobile No",fg="red")
     label_4 = Label(root, text="Email",fg="red")
+    label_5 = Label(root, text="password", fg="red")
+    label_6 = Label(root, text="Gender(M/F)", fg="red")
 
-    entry_1 = Entry(root)
-    entry_2 = Entry(root)
-    entry_3 = Entry(root)
-    entry_4 = Entry(root)
+    entry_1 = Entry(root,)
+    entry_2 = Entry(root,)
+    entry_3 = Entry(root,)
+    entry_4 = Entry(root,)
+    entry_5 = Entry(root, )
+    entry_6 = Entry(root, )
 
     label_1.grid(row=0, sticky=E)
     label_2.grid(row=1, sticky=E)
@@ -82,14 +124,61 @@ def Register():
     label_4.grid(row=3, sticky=E)
     entry_3.grid(row=2, column=1)
     entry_4.grid(row=3, column=1)
-    checkbox = Checkbutton(root, text="male")
-    checkbox.grid(columnspan=2)
-    checkbox = Checkbutton(root, text="female")
-    checkbox.grid(columnspan=2)
-    checkbox = Checkbutton(root, text="Keep me logged in")
-    checkbox.grid(columnspan=2)
-    MyButton1 = Button(root, text="Submit", width=10,command=showsubmit,bg="blue",fg="white")
-    MyButton1.grid(columnspan=2)
+    label_4.grid(row=4, sticky=E)
+    entry_4.grid(row=4, column=1)
+    label_5.grid(row=5, sticky=E)
+    entry_5.grid(row=5, column=1)
+    label_6.grid(row=6, sticky=E)
+    entry_6.grid(row=6, column=1)
+
+
+
+
+
+    def submit():
+        name = entry_1.get()
+        name2 = entry_2.get()
+        name3 = entry_3.get()
+        name4 = entry_4.get()
+
+        name5 = entry_5.get()
+        name6 = entry_6.get()
+
+        with open(name+".txt", "w") as wf:
+            with open(name2 + ".txt", "w") as wff:
+                with open(name3 + ".txt", "w") as wff3:
+                    with open(name4 + ".txt", "w") as wff4:
+                        with open(name5 + ".txt", "w") as wff5:
+                            with open(name6 + ".txt", "w") as wff6:
+
+                                name = entry_1.get()
+                                name2 = entry_2.get()
+                                name3 = entry_3.get()
+                                name4 = entry_4.get()
+                                name5 = entry_5.get()
+                                name6 = entry_6.get()
+
+                                wf.write(name+'\n')
+                                wf.write(name2+'\n')
+                                wf.write(name3+'\n')
+                                wf.write(name4+'\n')
+                                wf.write(name5+'\n')
+                                wf.write(name6+'\n')
+
+                        wf.close()
+                        wff.close()
+                        wff3.close()
+                        wff4.close()
+                        wff5.close()
+                        wff6.close()
+                        messagebox.showinfo('info','thanks for register')
+        root.destroy()
+
+    MyButton1 = Button(root, text="Submit", width=10,command=submit,bg="blue",fg="white")
+    MyButton1.grid(row=9,column=1)
+
+
+
 
     mainloop()
 
